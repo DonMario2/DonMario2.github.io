b335aaa439e4d2aba74f48fe53a9f823c0d8841b
Mario Bravo <mario.bravo.82@gmail.com>
2023-11-15 18:43:08 -0300

    modificacion general 15/11

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ $(document).ready(function(){
         $('#menu').remoteClass('fa-times');
         $('header').remoteClass('toggle');
     });
+    $('a[href*="#').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+        );
+    });
 }); 
